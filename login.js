b6ed6564a260604d418f320c710a9b6b4de648a4
sheeja22 <45845291+sheeja22@users.noreply.github.com>
2018-12-13 15:18:19 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+function validate form(){
+x=document grt element BYZd("email")value
+if(x--"){
+alert("email must be filled");
+return false;
+}
+function validate form(){
+x=document grt element BYZd("password")value
+if(x--"){
+alert("password must be filled");
+return false;
+}
