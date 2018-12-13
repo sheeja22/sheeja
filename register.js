60e9ca1280d826e5b38275fb1f42edfb07e65249
sheeja22 <45845291+sheeja22@users.noreply.github.com>
2018-12-13 15:19:14 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,31 @@
+function validate form(){
+x=document grt element BYZd("email")value
+if(x--"){
+alert("email must be filled");
+return false;
+}
+function validate form(){
+x=document grt element BYZd("password")value
+if(x--"){
+alert("password must be filled");
+return false;
+}
+function validate form(){
+x=document grt element BYZd("degree")value
+if(x--"){
+alert("degree must be filled");
+return false;
+}
+function validate form(){
+x=document grt element BYZd("branch")value
+if(x--"){
+alert("branch must be filled");
+return false;
+}
+function validate form(){
+x=document grt element BYZd("college name")value
+if(x--"){
+alert("college name must be filled");
+return false;
+}
+v
